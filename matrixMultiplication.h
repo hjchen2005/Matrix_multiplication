d20ebb4503aeaee5a463a8b3cf55b8cc1cb3d1ea
hjchen2005 <hjchen2005@gmail.com>
2015-04-28 18:23:47 -0400

    Update matrixMultiplication.h

--- matrixMultiplication.h
+++ matrixMultiplication.h
@@ -6,10 +6,10 @@
 #define m 8
 #define n 8
 
-void multiplication(int i, int j, int c[][8], int a[][8], int b[][8]);
-void multiplication(int i, int j, int c[][8], int a[][8], int b[][8]){
-	for (i = 0; i<x; i++){
-		for (j = 0; j<n; j++){
+void multiplication(int**,int**,int**);
+void multiplication(int** c,int** b,int** a){
+	for (int i = 0; i<x; i++){
+		for (int j = 0; j<n; j++){
 			c[i][j] = 0;
 			for (int k = 0; k<m; k++){
 				c[i][j] = c[i][j] + a[i][k] * b[k][j];
