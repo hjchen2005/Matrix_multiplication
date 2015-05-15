f1b535c5eaee9fcc76d60b69ca30d91014ff3087
hjchen2005 <hjchen2005@gmail.com>
2015-05-15 09:26:26 -0400

    Update matrix_multiplication_dpps.h

--- matrix_multiplication_dpps.h
+++ matrix_multiplication_dpps.h
@@ -3,14 +3,14 @@
 
 #include <smmintrin.h>
 
-#define size 4
+const int size= 4;
 
 void matrix_multiplication_dpps(float A[size][size], float B[size][size], float S[size][size]);
 
 void matrix_multiplication_dpps(float A[size][size], float B[size][size], float S[size][size]){
     const int mask = 0x1F;
-    __m128 v1 = { 0.0, 0.0, 0.0, 0.0 }, v2 = { 0.0, 0.0, 0.0, 0.0 },
-    v3 = { 0.0, 0.0, 0.0, 0.0 }, v4 = { 0.0, 0.0, 0.0, 0.0 }, s = { 0.0, 0.0, 0.0, 0.0 };
+    __m128 v1 = { 3, 2, 3, 2 }, v2 = { 2, 3, 2, 3 },
+    v3 = { 3, 2, 3, 2 }, v4 = { 2, 3, 2, 3 }, s = { 0.0, 0.0, 0.0, 0.0 };
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             for (int k = 0; k < size; k += 8) {
